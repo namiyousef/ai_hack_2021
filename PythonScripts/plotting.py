48ffd6bb211ebdfa186c560435d1174463a77ad8
Yousef <namiyousef@hotmail.com>
2021-02-21 03:26:43 +0100

    #10 added docstrings to plotting function

--- PythonScripts/plotting.py
+++ PythonScripts/plotting.py
@@ -1,42 +1,60 @@
-
-def plot_classes(X,y, vars = None, **kwargs):
-
+def plotting(x, activation = None, normalization = None, plot = True, **kwargs):
     """
-    Given X and y, where X represents the X values and y the classes, plots outcomes
-    with different colors.
+    Function to plot distributions for x depending on the type of normalization (transformation)
+    and activation applied.
 
-    I want to have the option of choosing which variables to plot against each other
-    NOTE:
-    -----
-    - lacks the use of **kwargs *args properly
-    - does not have functionality for x, y and title labels
-    - does not have legend
-    - only 2 dimensional
+    Dependencies:
+    -------------
 
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import pandas as pd
+
+    Parameters:
+    -----------
+    x : pd.DataFrame
+        dataframe of the data whose boxplot is to be plotted
+
+    activation : func
+        callable function that applies a transformation to x
+
+    normalization : sklearn.preprocessing item
+        a scaler from sklearn (can be MinMax, Robust, etc)
+
+    plot : bool
+        can be set to false to prevent plots (i.e when optimising)
+
+    **kwargs
+        kwargs for the activation functions
     """
-    if vars:
-        pass # here add code for custom variables
+    x_cols = x.columns
+    x = x.copy()
+    x = np.asarray(x)
+
+    if activation:
+        x = activation(x, **kwargs)
+        activation = activation.__name__.upper()
     else:
-        ncols = int(X.shape[1] ** (1/2))
-        nrows = X.shape[1] // ncols
+        activation = 'NONE'
 
-    fig, axes = plt.subplots(nrows = nrows, ncols = ncols, figsize = (nrows * 8, ncols * 8))
-    feature_1 = 0
-    feature_2 = feature_1 + 1
-    for j, column in enumerate(axes):
-        for i, row in enumerate(column):
-            for y_unique in np.unique(y):
-                axes[j,i].plot(
-                    X[y == y_unique, feature_1],
-                    X[y == y_unique, feature_2],
-                    '.'
-                )
-                axes[j,i].set_xlabel('Feature {}'.format(feature_1))
-                axes[j,i].set_ylabel('Feature {}'.format(feature_2))
 
-            feature_2 += 1
-            if feature_2 >= X.shape[1]:
-                feature_1 += 1
-                feature_2 = feature_1 + 1
-    plt.legend([*np.unique(y)])
-    return fig, axes
+    if normalization:
+        x = normalization().fit_transform(x)
+        normalization = normalization.__name__.upper()
+    else:
+        normalization = 'NONE'
+
+    if plot:
+        fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (16,8))
+        fig.suptitle(('The distribution of the data with {} normalization for an activation'
+                      ' function of {}'.format(normalization ,activation)), fontsize=16)
+
+        for i in range(x.shape[1]):
+            ax[0].hist(x[:,i], alpha = 0.2)
+        ax[0].legend(x_cols)
+        ax[1] = sns.boxplot(data = x)
+        plt.xticks(rotation=15)
+        plt.show()
+
+    return pd.DataFrame(data = x, columns = x_cols), kurtosis(x), skew(x)
+
